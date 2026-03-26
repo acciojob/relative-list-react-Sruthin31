7cf75835cb25845d6209909ac4eeb1b86d0b0a17
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-03-26 07:30:39 +0000

    updated react assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,17 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key="relativeList">
+                <li key="relativeListItem1">Mom</li>
+                 <li key="relativeListItem1">Dad</li>
+                  <li key="relativeListItem1">Sister</li>
+                   <li key="relativeListItem1">Brother</li>
+                    <li key="relativeListItem1">Aunt</li>
+                     <li key="relativeListItem1">Uncle</li>
+                      <li key="relativeListItem1">Cousins</li>
+
+               </ol>
+
     </div>
   )
 }
